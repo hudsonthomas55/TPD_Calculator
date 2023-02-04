77a6290d7613e9b1fed7f98d8b815b72d6671a61
Thomas <112832846+hudsonthomas55@users.noreply.github.com>
2023-02-04 13:47:59 -0500

    update main.py
    began process to make program GUI

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from tkinter import *
+
+# ---------------------------------------------- WINDOW SETUP ---------------------------------------------- #
+window = Tk()
+window.minsize(width=500, height=500)
+window.title("TPD Calculator")
+window.config(padx=50, pady=50)
+
+
+# -------------------------------------------- FUNCTIONS---------------------------------------- #
+def clear_frame():
+    for widget in window.winfo_children():
+        widget.destroy()
+
+
+# -------------------------------------------- CODE ---------------------------------------- #
 aww = input("Enter pre-injury AWW (no commas): \n$")
 date_from = input("Enter starting date (MM/DD/YY): \n")
 date_to = input("Enter ending date (MM/DD/YY): \n")
@@ -13,5 +29,14 @@ print(f"TPD owed: {tpd_owed}. Copy/paste below message:")
 print(message)
 
 
+def main_screen():
+    clear_frame()
+    welcome_label = Label(text="Welcome! \n Let's calculate some TPD. \n", font=("Arial", 30))
+    welcome_label.grid(row=0, column=0, columnspan=3)
+
+
+# ---------------------------------------- KEEP WINDOW OPEN ---------------------------------------- #
+main_screen()
+window.mainloop()
 
 
