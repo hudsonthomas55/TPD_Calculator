6cc1b6fb01058a18fd729da6445a50531c298d66
Thomas <112832846+hudsonthomas55@users.noreply.github.com>
2023-02-06 15:06:55 -0500

    Update main.py
    completed calculation function to return within GUI window. Allowed user to input special characters like '$' and ',' in the dollar inputs - created strip feature.

--- main.py
+++ main.py
@@ -19,40 +19,43 @@ def clear_frame():
         widget.destroy()
 
 
-def tpd_calc():
-    tpd_owed = ((float(aww) - float(earnings)) * .6667)
-    tpd_rounded = round(tpd_owed, 2)
-    print(f"TPD calculated for date range: {date_from}-{date_to}. AWW was ${aww}, earnings were ${earnings}. TPD owed "
-          f"for this period is ${tpd_owed}. TPD paid is: ${tpd_rounded}.")
 
 
 # ------------------------------------- MAIN SCREEN ------------------------------------- #
 def main_screen():
     clear_frame()
 
-    # SUBMIT BUTTON
+    # Submit button function
     def submit_button():
         global aww
         global date_from
         global date_to
         global earnings
 
+        # Get user inputs
         aww_answer = aww_entry.get()
         date_from_answer = date_from_entry.get()
         date_to_answer = date_to_entry.get()
         earnings_answer = earnings_entry.get()
 
-        print(f"You entered the aww as ${aww_answer}, date range as {date_from_answer}-{date_to_answer}, "
-              f"and the earnings during this time was ${earnings_answer}.")
-
-        aww = aww_answer
+        # Update global variables
+        aww = aww_answer.replace("$", "").replace(",", "")
         date_from = date_from_answer
         date_to = date_to_answer
-        earnings = earnings_answer
+        earnings = earnings_answer.replace("$", "").replace(",", "")
 
+        # Run TPD calculation based on user inputs
         tpd_calc()
 
+    def tpd_calc():
+        tpd_owed = ((float(aww) - float(earnings)) * .6667)
+        tpd_rounded = round(tpd_owed, 2)
+        return_label.config(text=f"TPD calculated for date range: {date_from}-{date_to}. \n"
+                                 f"AWW was ${aww}, earnings were ${earnings}. \n"
+                                 f"TPD owed for this period is ${tpd_owed}. \n"
+                                 f"TPD paid is: ${tpd_rounded}. \n")
 
+    # Screen setup
     welcome_label = Label(text="Welcome! \n Let's calculate some TPD. \n\n", font=("Arial", 30))
     welcome_label.grid(row=0, column=0, columnspan=3)
 
@@ -60,7 +63,7 @@ def main_screen():
     aww_label = Label(text="Enter the pre-injury average weekly wage: ")
     aww_label.grid(row=1, column=0)
     aww_entry = Entry(width=30)
-    aww_entry.insert(END, string="Do not include special characters ('$' or ',')")
+    aww_entry.insert(END, string="$1,000")
     aww_entry.grid(row=1, column=1, columnspan=3)
     aww_entry.focus_set()
 
@@ -80,14 +83,16 @@ def main_screen():
     earnings_label = Label(text="How much did the employee earn during this period? ")
     earnings_label.grid(row=3, column=0)
     earnings_entry = Entry(width=30)
-    earnings_entry.insert(END, string="Do not include special characters ('$' or ',')")
+    earnings_entry.insert(END, string="$500")
     earnings_entry.grid(row=3, column=1, columnspan=3)
 
     # Submit Button
-    spacer_label = Label(text="\n")
+    spacer_label = Label(text="\n\n")
     spacer_label.grid(row=4, column=0)
+    return_label = Label(text="", justify=LEFT, anchor="w")
+    return_label.grid(row=5, column=0, columnspan=4)
     submit_button = Button(text="Calculate TPD", width=30, command=submit_button)
-    submit_button.grid(row=5, column=0, columnspan=2)
+    submit_button.grid(row=6, column=0, columnspan=2)
 
 
 # ---------------------------------------- KEEP WINDOW OPEN ---------------------------------------- #
